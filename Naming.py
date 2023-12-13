f0884374ab6b0d3b0e8c9207143c8b2e8874bb8f
cheeseOFcheese <96741794+cheeseOFcheese@users.noreply.github.com>
2023-12-13 11:23:03 +0300

    Update Naming.py
    extrude option

--- Naming.py
+++ Naming.py
@@ -1,7 +1,7 @@
 import bpy
 
 # Путь к шрифту Arial с поддержкой кириллицы на вашем компьютере
-arial_cyrillic_font_path = "C:/Windows/Fonts/Arial.ttf"  # Замените это на фактический путь к шрифту Arial или другому шрифту с кириллицей
+arial_cyrillic_font_path = "C:/Windows/Fonts/Arial.ttf"  # Измените на путь к вашему шрифту Arial или другому шрифту с кириллицей
 
 # Функция для проверки наличия шрифта в Blender и его загрузки
 def get_font():
@@ -21,65 +21,48 @@ def get_or_create_material():
         material_baze_text.diffuse_color = (0.0, 0.5, 1.0)  # Настройте цвет по вашему усмотрению
     return material_baze_text
 
-# Функция для создания 3D текста с указанным текстом и материалом
+# Функция для создания текста с заданным текстом, шрифтом и материалом
 def create_text(text, font, material):
     bpy.ops.object.text_add()
     text_obj = bpy.context.object
     text_obj.data.body = text
     text_obj.data.font = font
-    
-    # Применяем материал к текстовому объекту
     text_obj.active_material = material
+    
+    # Устанавливаем параметр экструзии, привязанный к размеру текста (в пропорции 0.2)
+    initial_scale = text_obj.scale.copy()  # Запоминаем исходный размер текста
+    text_obj.data.extrude = initial_scale[0] * 0.2  # Привязываем экструзию к размеру текста
     
     return text_obj
 
-# Функция для проверки наличия шрифта и создания текста на гранях
+# Функция для создания текста на гранях выбранного объекта MESH
 def create_text_on_faces(selected_obj, material_baze_text):
     if selected_obj and selected_obj.type == 'MESH':
         mesh = selected_obj.data
-        
-        # Создаем материалы словарем для улучшения производительности
         materials_dict = {mat.name: mat for mat in selected_obj.data.materials}
-        
-        # Получаем шрифт
         font = get_font()
         
-        # Перебираем все грани меша
         for face in mesh.polygons:
-            # Получить материал, примененный к этой грани
             material_index = face.material_index
             
-            # Если у грани есть материал
             if material_index != -1:
                 face_material = materials_dict.get(selected_obj.data.materials[material_index].name)
                 
-                # Если материал найден
                 if face_material:
-                    # Получить имя материала
                     material_name = face_material.name
-                    
-                    # Рассчитываем центр грани
                     center = selected_obj.matrix_world @ face.center
-                    
-                    # Получаем нормаль к грани
                     normal = face.normal
                     
-                    # Создаем текст с названием материала и применяем материал
                     text_obj = create_text(material_name, font, material_baze_text)
-                    
-                    # Устанавливаем позицию текста в центре грани
                     text_obj.location = center
-                    
-                    # Выравниваем текст по нормали грани
                     text_obj.rotation_mode = 'QUATERNION'
                     text_obj.rotation_quaternion = normal.to_track_quat('Z', 'Y')
-                    
-                    # Масштабируем размер текста (опционально)
                     text_obj.scale *= 2  # Измените этот множитель по вашему усмотрению 
 
-# Создаем текст на гранях для всех выбранных объектов MESH
+# Получаем все выбранные объекты MESH
 selected_objects = [obj for obj in bpy.context.selected_objects if obj.type == 'MESH']
 material_baze_text = get_or_create_material()
 
+# Для каждого выбранного объекта MESH создаем текст на его гранях
 for selected_obj in selected_objects:
     create_text_on_faces(selected_obj, material_baze_text)
