3f8c917083ba6dafc6dc9389857fa75acf046c15
cheeseOFcheese <96741794+cheeseOFcheese@users.noreply.github.com>
2023-12-25 23:44:16 +0300

    Update Naming.py

--- Naming.py
+++ Naming.py
@@ -1,43 +1,42 @@
 import bpy
 
+bl_info = {
+    "name": "Text on Faces",
+    "blender": (2, 80, 0),
+    "category": "Object",
+}
+
 class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
     bl_idname = "object.text_on_faces_operator"
     bl_label = "Text on Faces"
 
-    text_size: bpy.props.FloatProperty(
-        name="Text Size",
-        description="Size of the generated text",
-        default=2.0,
-        min=0.1,
-        max=10.0
-    )
-
-    letter_spacing: bpy.props.FloatProperty(
-        name="Letter Spacing",
-        description="Spacing between letters",
-        default=0.1,
-        min=0.0,
-        max=1.0
-    )
-
     def execute(self, context):
-        arial_cyrillic_font_path = "C:/Windows/Fonts/Arial.ttf"
-        
-        # Остальной код без изменений...
-
+        # Ваш код здесь
         return {'FINISHED'}
+
+class TEXT_ON_FACES_PT_Panel(bpy.types.Panel):
+    bl_label = "Text on Faces Panel"
+    bl_idname = "TEXT_ON_FACES_PT_Panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
 
     def draw(self, context):
         layout = self.layout
         layout.operator("object.text_on_faces_operator")
-        layout.prop(self, "text_size")
-        layout.prop(self, "letter_spacing")
+
+classes = (
+    TEXT_ON_FACES_OT_operator,
+    TEXT_ON_FACES_PT_Panel,
+)
 
 def register():
-    bpy.utils.register_class(TEXT_ON_FACES_OT_operator)
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    bpy.utils.unregister_class(TEXT_ON_FACES_OT_operator)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
     register()
