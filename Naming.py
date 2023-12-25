532af6c56f0a24105082e8cb51118d32e252062d
cheeseOFcheese <96741794+cheeseOFcheese@users.noreply.github.com>
2023-12-25 23:38:06 +0300

    Update Naming.py

--- Naming.py
+++ Naming.py
@@ -3,22 +3,6 @@ import bpy
 class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
     bl_idname = "object.text_on_faces_operator"
     bl_label = "Text on Faces"
-    
-    text_size: bpy.props.FloatProperty(
-        name="Text Size",
-        description="Size of the generated text",
-        default=2.0,
-        min=0.1,
-        max=10.0
-    )
-
-    letter_spacing: bpy.props.FloatProperty(
-        name="Letter Spacing",
-        description="Spacing between letters",
-        default=0.1,
-        min=0.0,
-        max=1.0
-    )
 
     def execute(self, context):
         arial_cyrillic_font_path = "C:/Windows/Fonts/Arial.ttf"
@@ -29,8 +13,9 @@ class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
             if not font:
                 bpy.ops.font.open(filepath=arial_cyrillic_font_path)
                 font = bpy.context.object.data.font
-                bpy.data.fonts.remove(font, do_unlink=True)
-                font = bpy.data.fonts.get("Arial Regular")
+                if font:
+                    bpy.data.fonts.remove(font, do_unlink=True)
+                    font = bpy.data.fonts.get("Arial Regular")
             return font
         
         # Функция для получения или создания материала "BazeText"
@@ -41,23 +26,22 @@ class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
                 material_baze_text.diffuse_color = (0.0, 0.5, 1.0)  # Настройте цвет по вашему усмотрению
             return material_baze_text
         
-        # Функция для создания текста с заданным текстом, шрифтом, размером, межбуквенным интервалом и материалом
-        def create_text(text, font, material, size, spacing):
+        # Функция для создания текста с заданным текстом, шрифтом и материалом
+        def create_text(text, font, material):
             bpy.ops.object.text_add()
             text_obj = bpy.context.object
             text_obj.data.body = text
             text_obj.data.font = font
             text_obj.active_material = material
             
-            # Устанавливаем параметры размера и интервала между буквами текста
-            text_obj.scale *= size
-            text_obj.data.extrude = size * 0.2  # Привязываем экструзию к размеру текста
-            text_obj.data.space_character = spacing
+            # Устанавливаем параметр экструзии, привязанный к размеру текста (в пропорции 0.2)
+            initial_scale = text_obj.scale.copy()  # Запоминаем исходный размер текста
+            text_obj.data.extrude = initial_scale[0] * 0.2  # Привязываем экструзию к размеру текста
             
             return text_obj
         
         # Функция для создания текста на гранях выбранного объекта MESH
-        def create_text_on_faces(selected_obj, material_baze_text, size, spacing):
+        def create_text_on_faces(selected_obj, material_baze_text):
             if selected_obj and selected_obj.type == 'MESH':
                 mesh = selected_obj.data
                 materials_dict = {mat.name: mat for mat in selected_obj.data.materials}
@@ -74,10 +58,11 @@ class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
                             center = selected_obj.matrix_world @ face.center
                             normal = face.normal
                             
-                            text_obj = create_text(material_name, font, material_baze_text, size, spacing)
+                            text_obj = create_text(material_name, font, material_baze_text)
                             text_obj.location = center
                             text_obj.rotation_mode = 'QUATERNION'
                             text_obj.rotation_quaternion = normal.to_track_quat('Z', 'Y')
+                            text_obj.scale *= 2  # Измените этот множитель по вашему усмотрению 
         
         # Получаем все выбранные объекты MESH
         selected_objects = [obj for obj in bpy.context.selected_objects if obj.type == 'MESH']
@@ -85,21 +70,28 @@ class TEXT_ON_FACES_OT_operator(bpy.types.Operator):
         
         # Для каждого выбранного объекта MESH создаем текст на его гранях
         for selected_obj in selected_objects:
-            create_text_on_faces(selected_obj, material_baze_text, self.text_size, self.letter_spacing)
+            create_text_on_faces(selected_obj, material_baze_text)
         
         return {'FINISHED'}
+
+class TEXT_ON_FACES_PT_Panel(bpy.types.Panel):
+    bl_label = "Text on Faces Panel"
+    bl_idname = "TEXT_ON_FACES_PT_Panel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Tool'
 
     def draw(self, context):
         layout = self.layout
         layout.operator("object.text_on_faces_operator")
-        layout.prop(self, "text_size")
-        layout.prop(self, "letter_spacing")
 
 def register():
     bpy.utils.register_class(TEXT_ON_FACES_OT_operator)
+    bpy.utils.register_class(TEXT_ON_FACES_PT_Panel)
 
 def unregister():
     bpy.utils.unregister_class(TEXT_ON_FACES_OT_operator)
+    bpy.utils.unregister_class(TEXT_ON_FACES_PT_Panel)
 
 if __name__ == "__main__":
     register()
